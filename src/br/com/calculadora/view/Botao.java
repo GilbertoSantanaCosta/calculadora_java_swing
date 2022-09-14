41aa11aa303ad3ac8135a9ee6edda756592a68ce
GilbertoSantanaCosta <you@example.comg.s.d.c@hotmail.comgit config --global user.name Your>
2022-09-14 07:51:32 -0300

    Componente Botao

--- src/br/com/calculadora/view/Botao.java
+++ src/br/com/calculadora/view/Botao.java
@@ -1,10 +1,18 @@
 package br.com.calculadora.view;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import java.awt.*;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton{
 
-	
-
+	public Botao(String texto, Color cor){
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setFont(new Font("courier", Font.PLAIN, 10));
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		setForeground(Color.WHITE);
+	}
 }

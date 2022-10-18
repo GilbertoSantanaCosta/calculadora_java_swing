763200fca7a6f4a7f94bc8d9005595661f94c8b1
GilbertoSantanaCosta <you@example.comg.s.d.c@hotmail.comgit config --global user.name Your>
2022-10-18 07:35:58 -0300

    Calculadora V1.0

--- src/br/com/calculadora/modelo/Memoria.java
+++ src/br/com/calculadora/modelo/Memoria.java
@@ -48,11 +48,60 @@ public class Memoria {
 		}else if(tipoComando == TipoComando.NUMERO 
 			  || tipoComando == TipoComando.VIRGULA) {
 			
-			textoAtual = substituir ? texto : textoAtual + texto;
+			if(substituir == true ) {
+				
+				textoAtual = texto;
+				if(texto.equals(",")) {
+					textoAtual = "0,";
+				}
+				
+			}else {
+				
+				textoAtual = textoAtual + texto;
+				if(textoAtual.equals(",")) {
+					textoAtual = "0,";
+				}	
+			}
+			
+			//textoAtual = substituir ? texto : textoAtual + texto;
+			
 			substituir = false;
-		}
+		}else {
+			substituir = true;
+			textoAtual = obterResultadoOperacao();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
+			
+		} 
 		
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private String obterResultadoOperacao() {
+		if(ultimaOperacao == null || ultimaOperacao == TipoComando.IGUAL) {
+			return textoAtual;
+		}
+		
+		Double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+		Double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+		
+		Double resultado = 0.0;
+		
+		if(ultimaOperacao == TipoComando.SOMA) {	
+			resultado = numeroBuffer + numeroAtual;
+		}else if(ultimaOperacao == TipoComando.SUB) {
+			resultado = numeroBuffer - numeroAtual;
+		}else if(ultimaOperacao == TipoComando.DIV) {
+			resultado = numeroBuffer / numeroAtual;
+		}else if(ultimaOperacao == TipoComando.MULT) {
+			resultado = numeroBuffer * numeroAtual;
+		}
+		
+		
+		
+		String resultadoString = Double.toString(resultado).replace(".", ",");
+		boolean inteiro = resultadoString.endsWith(",0");
+		return inteiro ? resultadoString.replace(",0","") : resultadoString;
 	}
 
 	private TipoComando detectarTipoDeComando(String texto) {
@@ -78,7 +127,8 @@ public class Memoria {
 				return TipoComando.SUB;
 			}else if("=".equals(texto)) {
 				return TipoComando.IGUAL;
-			}else if(",".equals(texto) && !textoAtual.contains(",")) {
+			}else if(",".equals(texto) && 
+						!textoAtual.contains(",")) {
 				return TipoComando.VIRGULA;
 			}
 		}
